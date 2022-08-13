16b0a23d7416767886aab539328d0d50557377c3
Abduraimov Muminjon <muminjonguru@outlook.com>
2022-08-13 20:22:44 +0500

    src

---
+++ Unit1.pas
@@ -0,0 +1,54 @@
+unit Unit1;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, REST.Types,
+  FireDAC.Stan.Intf, FireDAC.Stan.Option, FireDAC.Stan.Param,
+  FireDAC.Stan.Error, FireDAC.DatS, FireDAC.Phys.Intf, FireDAC.DApt.Intf,
+  Data.DB, FireDAC.Comp.DataSet, FireDAC.Comp.Client, REST.Response.Adapter,
+  REST.Client, Data.Bind.Components, Data.Bind.ObjectScope,
+  FMX.Controls.Presentation, FMX.Edit, FMX.StdCtrls, FMX.Memo.Types,
+  FMX.ScrollBox, FMX.Memo;
+
+type
+  TForm1 = class(TForm)
+    RESTClient1: TRESTClient;
+    RESTRequest1: TRESTRequest;
+    RESTResponse1: TRESTResponse;
+    RESTResponseDataSetAdapter1: TRESTResponseDataSetAdapter;
+    FDMemTable1: TFDMemTable;
+    Edit1: TEdit;
+    Edit2: TEdit;
+    Button1: TButton;
+    Memo1: TMemo;
+    StyleBook1: TStyleBook;
+    procedure Button1Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.fmx}
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  RESTClient1.ResetToDefaults;
+  RESTClient1.Accept := 'application/json';
+  RESTClient1.AcceptCharset := 'UTF-8, *;q=0.8';
+  RESTClient1.BaseURL := 'https://api.apilayer.com/whois/query?apikey=' + Edit1.Text + '&domain=' + Edit2.Text;
+  RESTResponse1.ContentType := 'application/json';
+
+  RESTRequest1.Execute;
+
+  Memo1.Text := RESTResponse1.Content;
+end;
+
+end.
